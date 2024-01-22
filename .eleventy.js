6d9cbae707c37087d677912dbf7c66794bdeaaa4
David Rocendo <rocendod@msu.edu>
2024-01-22 12:33:07 -0500

    final touches

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,7 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/style.css");
     eleventyConfig.addPassthroughCopy("images");
-    eleventyConfig.addPassthroughCopy("./src/mood.md");
-    eleventyConfig.addCollection("allMyContent", function(collectionApi) {
-        return collectionApi.getAll();
-      });
-
+    eleventyConfig.addPassthroughCopy("./src/moon.md");
     return {
         dir: {
             input: "src",
